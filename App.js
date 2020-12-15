142d9ad7c62e22cf6f22d7c3257fba0edf4e7ec0
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2020-12-14 21:11:18 -0300

    Add files via upload
    tentando criar uma função para iniciar a animação pelo button

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { SafeAreaView, Button } from 'react-native';
-import LottieView from 'lottie-react-native'
+import React, {useState} from 'react';
+import { SafeAreaView, Button, Alert } from 'react-native';
+import Lottie from 'lottie-react-native'
 
 import kurama from './kurama.json';
 
 export default function App(){
+  const [animationState, setAnimationState] = useState({
+    isStopped: true, isPaused: false
+  });
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: false, 
+    
+  };
+
   return(
     <SafeAreaView style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
-        <LottieView
-        resizeMode="contain" autoSize source={kurama}/>
-        <Button/>
+        <Lottie options={defaultOptions} isStopped={animationState.isStopped} isPaused={animationState.isPaused} resizeMode="contain" autoSize source={kurama}/>
+        <Button title="Teste" /*onPress={() =>{
+          setAnimationState({
+            ...animationState,
+            isStopped: false
+          })
+        }}*/ onPress={() => Alert.alert('Simple Button pressed')}/>
     </SafeAreaView>
   );
 }
