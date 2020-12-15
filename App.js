8d55f74619d9ac5ec0b9c0709ad2876eb2c487c9
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2020-12-15 20:33:59 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -5,9 +5,7 @@ import Lottie from 'lottie-react-native'
 import kurama from './kurama.json';
 
 export default function App(){
-  const [animationState, setAnimationState] = useState({
-    isStopped: true, isPaused: false
-  });
+  const [animationState, setAnimationState] = useState({isStopped: true});
 
   const defaultOptions = {
     loop: false,
@@ -17,13 +15,16 @@ export default function App(){
 
   return(
     <SafeAreaView style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
-        <Lottie options={defaultOptions} isStopped={animationState.isStopped} isPaused={animationState.isPaused} resizeMode="contain" autoSize source={kurama}/>
-        <Button title="Teste" /*onPress={() =>{
+        
+        <Button title="Teste" onPress={() =>{
           setAnimationState({
             ...animationState,
             isStopped: false
+            
           })
-        }}*/ onPress={() => Alert.alert('Simple Button pressed')}/>
+          
+        }}/* onPress={() => Alert.alert('Simple Button pressed')}*//>
+        <Lottie /*options={defaultOptions}*/ isStopped={animationState.isStopped} isPaused={false} resizeMode="contain" autoSize source={kurama}/>
     </SafeAreaView>
   );
 }
